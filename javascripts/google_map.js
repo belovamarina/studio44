99e36c2f0922125b060666cb7ff63b81f8a696b3
Marina <belova.ma.ev@gmail.com>
2017-05-15 17:59:07 +0300

    Initial commit

---
+++ javascripts/google_map.js
@@ -0,0 +1,25 @@
+function init_map() {
+
+    var var_location = new google.maps.LatLng(55.654604, 37.416394);
+
+    var var_mapoptions = {
+        center: var_location,
+
+        zoom: 17
+    };
+
+    var var_marker = new google.maps.Marker({
+        position: var_location,
+        map: var_map,
+        title: "Studio44"
+    });
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"),
+        var_mapoptions);
+
+    var_marker.setMap(var_map);
+
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
+
